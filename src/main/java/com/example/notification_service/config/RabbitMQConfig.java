c77209493ac45d50eb20bcf399bc447c415ed0d4
Gideon0123 <nwokporochijiokegideon@gmail.com>
2026-06-19 09:09:17 -0700

    consumers 6

--- src/main/java/com/example/notification_service/config/RabbitMQConfig.java
+++ src/main/java/com/example/notification_service/config/RabbitMQConfig.java
@@ -1,12 +1,15 @@
 package com.example.notification_service.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.retry.RejectAndDontRequeueRecoverer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.interceptor.RetryOperationsInterceptor;
 
 @Configuration
 public class RabbitMQConfig {
@@ -231,7 +234,22 @@ public class RabbitMQConfig {
                 .with("password.reset.dlq");
     }
 
+    @Bean
+    public RetryOperationsInterceptor retryInterceptor() {
 
+        return RetryInterceptorBuilder
+                .stateless()
+                .maxAttempts(3)
+                .backOffOptions(
+                        1000,
+                        2.0,
+                        10000
+                )
+                .recoverer(
+                        new RejectAndDontRequeueRecoverer()
+                )
+                .build();
+    }
 
     @Bean
     public MessageConverter messageConverter() {
@@ -241,14 +259,18 @@ public class RabbitMQConfig {
     @Bean
     public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
             ConnectionFactory connectionFactory,
-            MessageConverter messageConverter
+            MessageConverter messageConverter,
+            RetryOperationsInterceptor retryInterceptor
     ) {
+
         SimpleRabbitListenerContainerFactory factory =
                 new SimpleRabbitListenerContainerFactory();
 
         factory.setConnectionFactory(connectionFactory);
 
         factory.setMessageConverter(messageConverter);
+
+        factory.setAdviceChain(retryInterceptor);
 
         return factory;
     }
